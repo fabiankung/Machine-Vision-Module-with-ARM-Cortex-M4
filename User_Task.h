87dd8abc7bd8385f35d4f830cc24e22265cc0e30
FK <fabiankung@users.noreply.github.com>
2018-05-06 16:15:22 +0800

    Add files via upload

---
+++ User_Task.h
@@ -0,0 +1,24 @@
+// Author			: Fabian Kung
+// Date				: 18 August 2018
+// Filename			: User_Task.h
+
+
+// Include common header to all drivers and sources.  Here absolute path is used.
+// To edit if one change folder
+#include "osmain.h"
+
+// 
+//
+// --- PUBLIC VARIABLES ---
+//
+
+
+//
+// --- PUBLIC FUNCTION PROTOTYPE ---
+//
+void Proce_MessageLoop_StreamImage(TASK_ATTRIBUTE *);
+void Proce_EyeLED_Effect(TASK_ATTRIBUTE *);
+void Proce_Image1(TASK_ATTRIBUTE *);
+void Proce_Image2(TASK_ATTRIBUTE *);
+void Proce_Image3(TASK_ATTRIBUTE *);
+//void Proce_Image4(TASK_ATTRIBUTE *);
